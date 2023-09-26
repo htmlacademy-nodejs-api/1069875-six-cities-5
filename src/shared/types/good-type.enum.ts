07b4b474e4692c56e4e91c72979e905412873549
ArinaKo <arinakovalenco7@gmail.com>
2023-09-26 16:15:58 +0300

    Добавит типы для импортируемых сущностей

---
+++ src/shared/types/good-type.enum.ts
@@ -0,0 +1,9 @@
+export enum GoodType {
+  Breakfast = 'Breakfast',
+  AirConditioning = 'Air conditioning',
+  Workspace = 'Laptop friendly workspace',
+  BabySeat = 'Baby seat',
+  Washer = 'Washer',
+  Towels = 'Towels',
+  Fridge = 'Fridge',
+}
