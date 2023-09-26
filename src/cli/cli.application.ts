c3c02827fa76830e61a44ff57cf494d5635a2524
ArinaKo <arinakovalenco7@gmail.com>
2023-09-26 14:51:25 +0300

    Добавит менеджеру метод processCommand

--- src/cli/cli.application.ts
+++ src/cli/cli.application.ts
@@ -1,4 +1,5 @@
 import { Command } from './commands/command.interface.js';
+import { CommandParser } from './command-parser.js';
 
 export class CLIApplication {
   private commands: Record<string, Command> = {};
@@ -13,5 +14,13 @@ export class CLIApplication {
 
       this.commands[commandName] = command;
     });
+  }
+
+  public processCommand(argv: string[]): void {
+    const parsedCommand = CommandParser.parse(argv);
+    const [commandName] = Object.keys(parsedCommand);
+    const command = this.commands[commandName];
+    const commandArguments = parsedCommand[commandName] ?? [];
+    command.execute(...commandArguments);
   }
 }
