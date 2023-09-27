e416a0b34a0486862e7af6e75f974a081fd68675
ArinaKo <arinakovalenco7@gmail.com>
2023-09-27 16:14:02 +0300

    Добавит команду ImportCommand

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,10 +1,11 @@
-import { CLIApplication, HelpCommand, VersionCommand } from './cli/index.js';
+import { CLIApplication, HelpCommand, VersionCommand, ImportCommand } from './cli/index.js';
 
 function bootstrap() {
   const cliApplication = new CLIApplication();
   cliApplication.registeredCommands([
     new HelpCommand(),
     new VersionCommand(),
+    new ImportCommand(),
   ]);
   cliApplication.processCommand(process.argv);
 }
