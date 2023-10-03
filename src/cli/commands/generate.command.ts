5fc6d3bc363525f0f20102fcc78d097bdd973f97
ArinaKo <arinakovalenco7@gmail.com>
2023-10-03 16:01:09 +0300

    Добавит команду GenerateCommand

---
+++ src/cli/commands/generate.command.ts
@@ -0,0 +1,50 @@
+import { appendFile } from 'node:fs/promises';
+import { TSVOfferGenerator } from '../../shared/libs/offer-generator/tsv-offer-generator.js';
+import { MockServerData } from '../../shared/types/mock-server-data.type.js';
+import { Command } from './command.interface.js';
+import got from 'got';
+
+export class GenerateCommand implements Command {
+  private initialData: MockServerData;
+
+  private async load(url: string) {
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      throw new Error(`Can't load data from ${url}`);
+    }
+  }
+
+  private async write(filepath: string, count: number) {
+    const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+
+    for (let i = 0; i < count; i++) {
+      await appendFile(
+        filepath,
+        `${tsvOfferGenerator.generate()}\n`,
+        { encoding: 'utf8' }
+      );
+    }
+  }
+
+  public getName(): string {
+    return '--generate';
+  }
+
+  public async execute(...params: string[]): Promise<void> {
+    const [count, filepath, url] = params;
+    const offerCount = Number.parseInt(count, 10);
+
+    try {
+      await this.load(url);
+      this.write(filepath, offerCount);
+      console.info(`File ${filepath} was created!`);
+    } catch (error: unknown) {
+      console.error('Can\'t generate data');
+
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+  }
+}
