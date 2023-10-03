c52c62c004a639dd64f22123249fdb2ffd6c19ea
ArinaKo <arinakovalenco7@gmail.com>
2023-10-03 23:48:54 +0300

    Рефакторит GenerateCommand

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,5 +1,5 @@
-import { appendFile } from 'node:fs/promises';
 import { TSVOfferGenerator } from '../../shared/libs/offer-generator/tsv-offer-generator.js';
+import { TSVFileWriter } from '../../shared/libs/file-writer/tsv-file-writer.js';
 import { MockServerData } from '../../shared/types/mock-server-data.type.js';
 import { Command } from './command.interface.js';
 import { getErrorMessage } from '../../shared/helpers/index.js';
@@ -18,13 +18,10 @@ export class GenerateCommand implements Command {
 
   private async write(filepath: string, count: number) {
     const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+    const tsvFileWriter = new TSVFileWriter(filepath);
 
     for (let i = 0; i < count; i++) {
-      await appendFile(
-        filepath,
-        `${tsvOfferGenerator.generate()}\n`,
-        { encoding: 'utf8' }
-      );
+      await tsvFileWriter.write(tsvOfferGenerator.generate());
     }
   }
 
