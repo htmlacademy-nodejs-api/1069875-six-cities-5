b421af3c67a9ded435bc9f5cc2afc3921485066c
ArinaKo <arinakovalenco7@gmail.com>
2023-10-03 16:49:43 +0300

    Добавит stream для чтения

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,12 +1,36 @@
 import { FileReader } from './file-reader.interface.js';
-import { readFileSync } from 'node:fs';
+import { createReadStream } from 'node:fs';
+import EventEmitter from 'node:events';
 
-export class TSVFileReader implements FileReader {
-  private rawData = '';
+const CHUNK_SIZE = 16300;
 
-  constructor(private readonly filename: string) {}
+export class TSVFileReader extends EventEmitter implements FileReader {
+  constructor(private readonly filename: string) {
+    super();
+  }
 
-  public read(): void {
-    this.rawData = readFileSync(this.filename, 'utf-8');
+  public async read(): Promise<void> {
+    const readStream = createReadStream(this.filename, {
+      highWaterMark: CHUNK_SIZE,
+      encoding: 'utf-8',
+    });
+
+    let remainingData = '';
+    let nextLineStart = -1;
+    let importedRowsCount = 0;
+
+    for await (const chunk of readStream) {
+      remainingData += chunk.toString();
+
+      while ((nextLineStart = remainingData.indexOf('\n')) >= 0) {
+        const completedData = remainingData.slice(0, ++nextLineStart);
+        remainingData = remainingData.slice(nextLineStart);
+        importedRowsCount++;
+
+        this.emit('line', completedData);
+      }
+    }
+
+    this.emit('end', importedRowsCount);
   }
 }
