344d392829812052b7a373d1a7d74833f97e9542
ArinaKo <arinakovalenco7@gmail.com>
2023-10-08 18:29:46 +0300

    Добавит класс приложения RestApplication и его инициализацию

---
+++ src/main.rest.ts
@@ -0,0 +1,12 @@
+import { RestApplication } from './rest/index.js';
+import { PinoLogger } from './shared/libs/logger/index.js';
+
+
+async function bootstrap() {
+  const logger = new PinoLogger();
+  const app = new RestApplication(logger);
+
+  app.init();
+}
+
+bootstrap();
