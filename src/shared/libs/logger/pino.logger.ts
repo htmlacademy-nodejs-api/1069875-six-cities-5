ba333034d7cc64347d1cb073e1a0fed83f7abfd4
ArinaKo <arinakovalenco7@gmail.com>
2023-10-09 23:54:09 +0300

    Внедрит зависимости

--- src/shared/libs/logger/pino.logger.ts
+++ src/shared/libs/logger/pino.logger.ts
@@ -2,7 +2,9 @@ import { resolve } from 'node:path';
 import { getCurrentModuleDirPath } from '../../helpers/index.js';
 import { Logger } from './logger.interface.js';
 import { Logger as PinoInstance, pino, transport } from 'pino';
+import { injectable } from 'inversify';
 
+@injectable()
 export class PinoLogger implements Logger {
   private readonly logger: PinoInstance;
 
