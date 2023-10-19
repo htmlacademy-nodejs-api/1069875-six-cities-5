88c00df9a75338bcfc3bf3289010544b90ac3fed
ArinaKo <arinakovalenco7@gmail.com>
2023-10-19 21:03:00 +0300

    Добавит имплементацию методов UserService

--- src/shared/modules/user/defaul-user.service.ts
+++ src/shared/modules/user/defaul-user.service.ts
@@ -21,4 +21,22 @@ export class DefaultUserService implements UserService {
     this.logger.info(`New user created: ${user.email}`);
     return result;
   }
+
+  public async findByEmail(email: string): Promise<DocumentType<UserEntity> | null> {
+    return this.userModel.findOne({ email }).exec();
+  }
+
+  public async findById(id: string): Promise<DocumentType<UserEntity> | null> {
+    return this.userModel.findById(id).exec();
+  }
+
+  public async findOrCreate(dto: CreateUserDTO, salt: string): Promise<DocumentType<UserEntity>> {
+    const existedUser = await this.findByEmail(dto.email);
+
+    if (existedUser) {
+      return existedUser;
+    }
+
+    return this.create(dto, salt);
+  }
 }
--- src/shared/modules/user/user-service.interface.ts
+++ src/shared/modules/user/user-service.interface.ts
@@ -4,6 +4,6 @@ import { CreateUserDTO, UserEntity } from './index.js';
 export interface UserService {
   create(dto: CreateUserDTO, salt: string): Promise<DocumentType<UserEntity>>;
   findOrCreate(dto: CreateUserDTO, salt: string): Promise<DocumentType<UserEntity>>;
-  findByEmail(dto: CreateUserDTO): Promise<DocumentType<UserEntity> | null>;
-  findById(dto: CreateUserDTO): Promise<DocumentType<UserEntity> | null>;
+  findByEmail(email: string): Promise<DocumentType<UserEntity> | null>;
+  findById(id: string): Promise<DocumentType<UserEntity> | null>;
 }
