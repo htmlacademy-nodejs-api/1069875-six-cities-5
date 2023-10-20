2e2e8f1b37493481d72258573b0ec44988f9db3d
ArinaKo <arinakovalenco7@gmail.com>
2023-10-20 13:43:13 +0300

    Добавит DefaultOfferService и OfferModel в контейнер

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -3,9 +3,10 @@ import { Container } from 'inversify';
 import { RestApplication, createRestApplicationContainer } from './rest/index.js';
 import { Component } from './shared/types/index.js';
 import { createUserContainer } from './shared/modules/user/index.js';
+import { createOfferContainer } from './shared/modules/offer/index.js';
 
 async function bootstrap() {
-  const appContainer = Container.merge(createRestApplicationContainer(), createUserContainer());
+  const appContainer = Container.merge(createRestApplicationContainer(), createUserContainer(), createOfferContainer());
 
   const app = appContainer.get<RestApplication>(Component.RestApplication);
   await app.init();
--- src/shared/modules/offer/default-offer.service.ts
+++ src/shared/modules/offer/default-offer.service.ts
@@ -1,14 +1,25 @@
-import { DocumentType } from '@typegoose/typegoose';
+import { DocumentType, types } from '@typegoose/typegoose';
+import { injectable, inject } from 'inversify';
 import { CreateOfferDTO } from './index.js';
 import { OfferService } from './offer-service.interface.js';
-import { OfferEntity, OfferModel } from './offer.entity.js';
+import { OfferEntity } from './offer.entity.js';
+import { Component } from '../../types/index.js';
+import { Logger } from '../../libs/logger/index.js';
 
+@injectable()
 export class DefaultOfferService implements OfferService {
+  constructor(
+    @inject(Component.Logger) private readonly logger: Logger,
+    @inject(Component.OfferModel) private readonly offerModel: types.ModelType<OfferEntity>,
+  ){}
+
   public async create(dto: CreateOfferDTO): Promise<DocumentType<OfferEntity>> {
-    return await OfferModel.create(dto);
+    const result = await this.offerModel.create(dto);
+    this.logger.info(`New offer created: ${dto.title}`);
+    return result;
   }
 
   public async findById(id: string): Promise<DocumentType<OfferEntity> | null> {
-    return OfferModel.findById(id).exec();
+    return this.offerModel.findById(id).exec();
   }
 }
