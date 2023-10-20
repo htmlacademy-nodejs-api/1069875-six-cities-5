b399b6a09279dc402d2c81186faed99ec81a82be
ArinaKo <arinakovalenco7@gmail.com>
2023-10-20 16:58:17 +0300

    Дополнит OfferEntity

--- src/shared/modules/offer/offer.entity.ts
+++ src/shared/modules/offer/offer.entity.ts
@@ -1,4 +1,4 @@
-import { prop, getModelForClass, defaultClasses, modelOptions, Ref } from '@typegoose/typegoose';
+import { prop, getModelForClass, defaultClasses, modelOptions, Ref, Severity } from '@typegoose/typegoose';
 import { City, GoodType, OfferType, Location } from '../../types/index.js';
 import { DescriptionLength, GuestsNumber, OfferTitleLength, PriceValue, RatingValue, RoomsNumber } from '../../const/offer.js';
 import { UserEntity } from '../user/user.entity.js';
@@ -9,6 +9,9 @@ export interface OfferEntity extends defaultClasses.Base {}
 @modelOptions({
   schemaOptions: {
     collection: 'offers'
+  },
+  options: {
+    allowMixed: Severity.ALLOW
   }
 })
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
@@ -18,109 +21,110 @@ export class OfferEntity extends defaultClasses.TimeStamps {
     minlength: OfferTitleLength.Min,
     maxlength: OfferTitleLength.Max,
   })
-  public title: string;
+  public title!: string;
 
   @prop({
     require: true,
     minlength: DescriptionLength.Min,
     maxlength: DescriptionLength.Max,
   })
-  public description: string;
+  public description!: string;
 
   @prop({
     require: true,
     default: new Date(),
   })
-  public date: Date;
+  public date!: Date;
 
   @prop({
     type: () => String,
     enum: City,
     require: true,
   })
-  public city: City;
+  public city!: City;
 
   @prop({
     require: true,
   })
-  public previewImage: string;
+  public previewImage!: string;
 
   @prop({
+    type: () => [String],
     require: true,
     default: [],
   })
-  public images: string[];
+  public images!: string[];
 
   @prop({
     require: true,
     default: false,
   })
-  public isPremium: boolean;
+  public isPremium!: boolean;
 
   @prop({
     require: true,
     default: false,
   })
-  public isFavorite: boolean;
+  public isFavorite!: boolean;
 
   @prop({
     require: true,
     min: RatingValue.Min,
     max: RatingValue.Max,
   })
-  public rating: number;
+  public rating!: number;
 
   @prop({
     type: () => String,
     enum: OfferType,
     require: true,
   })
-  public type: OfferType;
+  public type!: OfferType;
 
   @prop({
     require: true,
     min: RoomsNumber.Min,
     max: RoomsNumber.Max,
   })
-  public bedrooms: number;
+  public bedrooms!: number;
 
   @prop({
     require: true,
     min: GuestsNumber.Min,
     max: GuestsNumber.Max,
   })
-  public maxGuests: number;
+  public maxGuests!: number;
 
   @prop({
     require: true,
     min: PriceValue.Min,
     max: PriceValue.Max,
   })
-  public price: number;
+  public price!: number;
 
   @prop({
-    type: () => Array<string>,
+    type: () => [String],
     enum: GoodType,
     require: true,
     default: []
   })
-  public goods: GoodType[];
+  public goods!: GoodType[];
 
   @prop({
     ref: UserEntity,
     require: true,
   })
-  public hostId: Ref<UserEntity>;
+  public hostId!: Ref<UserEntity>;
 
   @prop({
     default: 0,
   })
-  public commentsNumber: number;
+  public commentsNumber?: number;
 
   @prop({
     require: true,
   })
-  public location: Location;
+  public location!: Location;
 }
 
 export const OfferModel = getModelForClass(OfferEntity);
