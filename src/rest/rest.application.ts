85cffd511616edbaa131da6f3dc1c80a0d884bd4
ArinaKo <arinakovalenco7@gmail.com>
2023-11-05 15:12:50 +0300

    Добавит регистрацию контроллеров

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -5,6 +5,7 @@ import { Logger } from '../shared/libs/logger/index.js';
 import { Component } from '../shared/types/component.enum.js';
 import { DatabaseClient } from '../shared/libs/database-client/index.js';
 import { getMongoURI } from '../shared/helpers/index.js';
+import { Controller } from '../shared/libs/rest/index.js';
 
 @injectable()
 export class RestApplication {
@@ -13,7 +14,8 @@ export class RestApplication {
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
-    @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient
+    @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
+    @inject(Component.OfferController) private readonly offerController: Controller,
   ){
     this.server = express();
   }
@@ -35,12 +37,20 @@ export class RestApplication {
     this.server.listen(port);
   }
 
+  private async _initControllers() {
+    this.server.use('/offers', this.offerController.router);
+  }
+
   public async init() {
     this.logger.info('Application initialization');
 
     this.logger.info('Init database...');
     await this._initDB();
     this.logger.info('Init database completed');
+
+    this.logger.info('Init controllers...');
+    await this._initControllers();
+    this.logger.info('Controllers initialization completed');
 
     this.logger.info('Try to init server…');
     await this._initServer();
