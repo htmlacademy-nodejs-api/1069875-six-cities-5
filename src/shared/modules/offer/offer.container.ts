afe79701a4ac4297f200e286d1122d87aadabb22
ArinaKo <arinakovalenco7@gmail.com>
2023-11-05 15:07:13 +0300

    Добавит OfferController в контейнер

--- src/shared/modules/offer/offer.container.ts
+++ src/shared/modules/offer/offer.container.ts
@@ -1,12 +1,14 @@
 import { Container } from 'inversify';
 import { types } from '@typegoose/typegoose';
 import { Component } from '../../types/index.js';
-import { DefaultOfferService, OfferEntity, OfferModel, OfferService } from './index.js';
+import { DefaultOfferService, OfferController, OfferEntity, OfferModel, OfferService } from './index.js';
+import { Controller } from '../../libs/rest/index.js';
 
 export function createOfferContainer() {
   const container = new Container();
   container.bind<OfferService>(Component.OfferService).to(DefaultOfferService).inSingletonScope();
   container.bind<types.ModelType<OfferEntity>>(Component.OfferModel).toConstantValue(OfferModel);
+  container.bind<Controller>(Component.OfferController).to(OfferController).inSingletonScope();
 
   return container;
 }
