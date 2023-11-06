0c17c14cbd49cf6004306c058fcbeb9d8e485c20
ArinaKo <arinakovalenco7@gmail.com>
2023-11-06 15:46:01 +0300

    Добавит контроллер UserController

---
+++ src/shared/modules/user/user.controller.ts
@@ -0,0 +1,73 @@
+import { inject } from 'inversify';
+import {
+  DefaultController,
+  HttpError,
+  HttpMethod,
+} from '../../libs/rest/index.js';
+import { Component } from '../../types/index.js';
+import { Logger } from '../../libs/logger/index.js';
+import { StatusCodes } from 'http-status-codes';
+import { Request, Response } from 'express';
+
+export class UserController extends DefaultController {
+  constructor(
+    @inject(Component.Logger) protected readonly logger: Logger,
+  ) {
+    super(logger);
+
+    this.logger.info('Register routes for UserController…');
+
+    this.addRoute({
+      path: '/register',
+      method: HttpMethod.Post,
+      handler: this.register,
+    });
+    this.addRoute({
+      path: '/login',
+      method: HttpMethod.Get,
+      handler: this.checkAuth,
+    });
+    this.addRoute({
+      path: '/login',
+      method: HttpMethod.Post,
+      handler: this.login,
+    });
+    this.addRoute({
+      path: '/logout',
+      method: HttpMethod.Delete,
+      handler: this.logout,
+    });
+  }
+
+  public register(_req: Request, _res: Response): void {
+    throw new HttpError(
+      StatusCodes.NOT_IMPLEMENTED,
+      'Not implemented',
+      'UserController'
+    );
+  }
+
+  public checkAuth(_req: Request, _res: Response): void {
+    throw new HttpError(
+      StatusCodes.NOT_IMPLEMENTED,
+      'Not implemented',
+      'UserController'
+    );
+  }
+
+  public login(_req: Request, _res: Response): void {
+    throw new HttpError(
+      StatusCodes.NOT_IMPLEMENTED,
+      'Not implemented',
+      'UserController'
+    );
+  }
+
+  public logout(_req: Request, _res: Response): void {
+    throw new HttpError(
+      StatusCodes.NOT_IMPLEMENTED,
+      'Not implemented',
+      'UserController'
+    );
+  }
+}
