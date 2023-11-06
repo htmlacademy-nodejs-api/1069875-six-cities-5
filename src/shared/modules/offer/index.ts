61fd6cf8abd5efeb5c903baa8ba622161e15562d
ArinaKo <arinakovalenco7@gmail.com>
2023-11-06 15:30:32 +0300

    Добавит тип CreateOfferRequest

--- src/shared/modules/offer/index.ts
+++ src/shared/modules/offer/index.ts
@@ -7,3 +7,4 @@ export * from './offer.container.js';
 export * from './offer.const.js';
 export * from './offer.controller.js';
 export * from './rdo/index.js';
+export * from './create-offer-request.type.js';
