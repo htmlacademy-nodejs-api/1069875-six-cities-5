60d0ba45d4065471faa4275361cc04a77c090713
ArinaKo <arinakovalenco7@gmail.com>
2023-11-11 15:36:31 +0300

    Добавит в контейнер и зарегистрирует CommentController

--- src/shared/modules/comment/comment.container.ts
+++ src/shared/modules/comment/comment.container.ts
@@ -1,12 +1,14 @@
 import { Container } from 'inversify';
 import { types } from '@typegoose/typegoose';
 import { Component } from '../../types/index.js';
-import { DefaultCommentService, CommentEntity, CommentModel, CommentService } from './index.js';
+import { DefaultCommentService, CommentEntity, CommentModel, CommentService, CommentController } from './index.js';
+import { Controller } from '../../libs/rest/index.js';
 
 export function createCommentContainer() {
   const container = new Container();
   container.bind<CommentService>(Component.CommentService).to(DefaultCommentService).inSingletonScope();
   container.bind<types.ModelType<CommentEntity>>(Component.CommentModel).toConstantValue(CommentModel);
+  container.bind<Controller>(Component.CommentController).to(CommentController).inSingletonScope();
 
   return container;
 }
--- src/shared/modules/comment/comment.controller.ts
+++ src/shared/modules/comment/comment.controller.ts
@@ -1,10 +1,11 @@
-import { inject } from 'inversify';
+import { inject, injectable } from 'inversify';
 import { DefaultController, HttpMethod } from '../../libs/rest/index.js';
 import { Component } from '../../types/index.js';
 import { Logger } from '../../libs/logger/index.js';
 import { Response } from 'express';
 import { CreateCommentRequest } from './index.js';
 
+@injectable()
 export class CommentController extends DefaultController {
   constructor(@inject(Component.Logger) protected readonly logger: Logger) {
     super(logger);
