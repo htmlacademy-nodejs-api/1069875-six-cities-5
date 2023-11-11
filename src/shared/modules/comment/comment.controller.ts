fbc161ca162756548a0782248035b5e6491b8808
ArinaKo <arinakovalenco7@gmail.com>
2023-11-11 15:21:47 +0300

    Добавит CommentController

---
+++ src/shared/modules/comment/comment.controller.ts
@@ -0,0 +1,18 @@
+import { inject } from 'inversify';
+import { DefaultController, HttpMethod } from '../../libs/rest/index.js';
+import { Component } from '../../types/index.js';
+import { Logger } from '../../libs/logger/index.js';
+import { Response } from 'express';
+import { CreateCommentRequest } from './index.js';
+
+export class CommentController extends DefaultController {
+  constructor(@inject(Component.Logger) protected readonly logger: Logger) {
+    super(logger);
+
+    this.logger.info('Register routes for CommentController…');
+
+    this.addRoute({ path: '/', method: HttpMethod.Post, handler: this.create });
+  }
+
+  public create(_req: CreateCommentRequest, _res: Response): void {}
+}
