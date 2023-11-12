5884a3cfe6f62afa0d17ba1831191188a76de039
ArinaKo <arinakovalenco7@gmail.com>
2023-11-12 21:34:52 +0300

    Добавит валидаторы для LoginDTO

--- src/shared/modules/user/dto/login.dto.ts
+++ src/shared/modules/user/dto/login.dto.ts
@@ -1,4 +1,14 @@
+import { IsEmail, IsString, Length } from 'class-validator';
+import { UserValidationMessage } from '../index.js';
+import { UserPasswordLength } from '../../../const/index.js';
+
 export class LoginDTO {
+  @IsEmail({}, { message: UserValidationMessage.email.invalidFormat })
   public email: string;
+
+  @IsString({ message: UserValidationMessage.password.invalidFormat })
+  @Length(UserPasswordLength.Min, UserPasswordLength.Max, {
+    message: UserValidationMessage.password.length,
+  })
   public password: string;
 }
