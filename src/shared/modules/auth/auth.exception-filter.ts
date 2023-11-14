6a16a6141366517dde86473bb7cbcf340a05435b
ArinaKo <arinakovalenco7@gmail.com>
2023-11-14 19:12:05 +0300

    Добавит AuthExceptionFilter

---
+++ src/shared/modules/auth/auth.exception-filter.ts
@@ -0,0 +1,27 @@
+import { inject } from 'inversify';
+import { Logger } from '../../libs/logger/index.js';
+import { ExceptionFilter } from '../../libs/rest/index.js';
+import { Component } from '../../types/index.js';
+import { Request, Response, NextFunction } from 'express';
+import { DefaultAuthException } from './index.js';
+
+export class AuthExceptionFilter implements ExceptionFilter {
+  constructor(@inject(Component.Logger) private readonly logger: Logger) {}
+
+  public catch(
+    error: Error,
+    _req: Request,
+    res: Response,
+    next: NextFunction
+  ): void {
+    if (!(error instanceof DefaultAuthException)) {
+      return next(error);
+    }
+
+    this.logger.error(`[AuthModule] ${error.message}`, error);
+    res.status(error.statusCode).json({
+      type: 'AUTHORIZATION',
+      message: error.message,
+    });
+  }
+}
--- src/shared/modules/auth/index.ts
+++ src/shared/modules/auth/index.ts
@@ -3,3 +3,4 @@ export * from './exceptions/index.js';
 export * from './auth.const.js';
 export * from './auth-service.interface.js';
 export * from './default-auth.service.js';
+export * from './auth.exception-filter.js';
