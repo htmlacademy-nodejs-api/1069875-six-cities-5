361b437be2596fd411c810aa08a8d96b02258c4f
ArinaKo <arinakovalenco7@gmail.com>
2023-11-15 15:47:59 +0300

    Добавит класс PathTransformer

---
+++ src/shared/libs/rest/transform/path-transformer.const.ts
@@ -0,0 +1,5 @@
+import { DEFAULT_AVATAR_FILE_NAME } from '../../../modules/user/user.const.js';
+
+export const DEFAULT_STATIC_IMAGES = [DEFAULT_AVATAR_FILE_NAME];
+
+export const STATIC_RESOURCE_FIELDS = ['avatarUrl'];
